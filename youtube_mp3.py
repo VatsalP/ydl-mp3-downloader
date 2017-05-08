32247a07f969c764a1aedc3ff06d6dba451c516f
Vatsal Parekh <vatsalbparekh@gmail.com>
2017-05-08 13:00:55 +0530

    Added some css

--- youtube_mp3.py
+++ youtube_mp3.py
@@ -1,24 +1,15 @@
 import logging
+import os
 
 import youtube_dl
 from bottle import Bottle, run, static_file, \
                 debug, request, mako_view as view
 
+
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
 logging.basicConfig(filename='ze.log', level=logging.DEBUG)
 app = Bottle()
-debug(True)
-
-
-@app.get('/')
-@view('index.html')
-def index():
-    return """
-        Enter URL:
-        <form method='post' action='/download'>
-            <input type='text' placeholder='Enter url' name='url'>
-            <input type="submit" value="Submit">
-        </form>
-        """
 
 
 class Logger(object):
@@ -31,9 +22,9 @@ class Logger(object):
     def error(self, msg):
         logging.error("Message: {}".format(msg))
 
+
 YDL_OPTS = {
     'format': 'bestaudio/best',
-#   'outtmpl': "%(title)s.%(ext)s",
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -43,6 +34,31 @@ YDL_OPTS = {
 }
 
 
+@app.get('/')
+@view('index.html')
+def index():
+    return
+    """
+    return 
+        Enter URL:
+        <form method='post' action='/download'>
+            <input type='text' placeholder='Enter url' name='url'>
+            <input type="submit" value="Submit">
+        </form>
+    """
+
+
+@app.get('/<filepath:path>')
+def static_files(filepath):
+    return static_file(
+        filepath,
+        root=dir_path+'/'+'static/'
+    )
+
+
+# regex for youtube links
+# ^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$
+# https://stackoverflow.com/questions/19377262/regex-for-youtube-url
 @app.post('/download')
 def download():
     """Serves mp3s for download
@@ -54,5 +70,5 @@ def download():
     filename = info['title'] + '-' + info['id'] + '.mp3'
     return static_file(filename, root=".", download=filename)
 
-if '__name__' == __main__:
+if __name__ == '__main__':
     run(app, reloader=True)
